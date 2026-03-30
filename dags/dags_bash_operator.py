24651544f7481d2c0c5a3a4ffa0a89f1386d41f2
jiwoongLee <leewo1001@gmail.com>
2026-03-30 23:41:03 +0900

    bash_operator dat 업로드

---
+++ dags/dags_bash_operator.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+import datetime
+
+import pendulum
+
+from airflow.providers.standard.operators.bash import BashOperator
+from airflow.providers.standard.operators.empty import EmptyOperator
+from airflow.sdk import DAG
+
+with DAG(
+    dag_id="example_bash_operator",
+    schedule="0 0 * * *",
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False, #위에 2023년 3월 1일일 때 1월 1일부터 3월 1일까지 돌릴것인지 설정, 1/1~2/28 한꺼번에 실행
+    tags=["example", "example2"],
+    params={"example_key": "example_value"},
+) as dag:
+    bash_t1 = BashOperator(
+        task_id="bash_t1",
+        bash_command="echo whoami",
+    )
+    bash_t2 = BashOperator(
+        task_id="bash_t2",
+        bash_command="echo $HOSTNAME",
+    )
+
+    bash_t1 >> bash_t2
