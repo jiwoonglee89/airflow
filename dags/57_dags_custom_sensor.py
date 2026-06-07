9fc5a838691f8a8423e5dbf0314ed1028c6b8f2f
jiwoongLee <leewo1001@gmail.com>
2026-06-07 22:16:37 +0900

    56강 업데이트

---
+++ dags/57_dags_custom_sensor.py
@@ -0,0 +1,32 @@
+#from sensors.56_seoul_api_date_sensor import SeoulApiDateSensor
+import pendulum
+import importlib
+# Airflow 3.0 부터 아래 경로로 import 합니다.
+from airflow.sdk import DAG
+
+module = importlib.import_module("sensors.56_seoul_api_date_sensor")
+SeoulApiDateSensor = module.SeoulApiDateSensor
+
+with DAG(
+    dag_id='dags_custom_sensor',
+    start_date=pendulum.datetime(2023,4,1, tz='Asia/Seoul'),
+    schedule=None,
+    catchup=False
+) as dag:
+    tb_corona_19_count_status_sensor = SeoulApiDateSensor(
+        task_id='tb_corona_19_count_status_sensor',
+        dataset_nm='TbCorona19CountStatus',
+        base_dt_col='S_DT',
+        day_off=0,
+        poke_interval=600,
+        mode='reschedule'
+    )
+    
+    tv_corona19_vaccine_stat_new_sensor = SeoulApiDateSensor(
+        task_id='tv_corona19_vaccine_stat_new_sensor',
+        dataset_nm='tvCorona19VaccinestatNew',
+        base_dt_col='S_VC_DT',
+        day_off=-1,
+        poke_interval=600,
+        mode='reschedule'
+    )
