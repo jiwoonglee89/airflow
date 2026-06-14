d8207737247626ef526ae1c17371d809b7de98f5
jiwoongLee <leewo1001@gmail.com>
2026-06-14 23:22:24 +0900

    60강 내용 수정

--- dags/60_dags_email_on_failure.py
+++ dags/60_dags_email_on_failure.py
@@ -4,6 +4,7 @@ import pendulum
 from datetime import timedelta
 # Airflow 3.0 부터 아래 경로로 import 합니다.
 from airflow.sdk import DAG, task, Variable
+from airflow.providers.smtp.notifications.smtp import SmtpNotifier
 
 # Airflow 2.10.5 이하 버전에서 실습시 아래 경로에서 import 하세요.
 #from airflow import DAG
@@ -15,15 +16,21 @@ email_str = Variable.get("email_target")
 email_lst = [email.strip() for email in email_str.split(',')]
 
 with DAG(
+    
     dag_id='dags_email_on_failure',
     start_date=pendulum.datetime(2023,5,1, tz='Asia/Seoul'),
     catchup=False,
     schedule='0 1 * * *',
     dagrun_timeout=timedelta(minutes=2),
-    default_args={
-        'email_on_failure': True,
-        'email': email_lst
-    }
+    # default_args={
+    #     'email_on_failure': True,
+    #     'email': email_lst
+    # } #기존 스타일
+    on_failure_callback=SmtpNotifier(
+        smtp_conn_id="con_smtp_gmail",
+        to=email_lst,
+        subject="[Airflow] {{ ti.task_id }} 태스크 실패 알림"
+    ) #제미나이 추천
 ) as dag:
     @task(task_id='python_fail')
     def python_task_func():
